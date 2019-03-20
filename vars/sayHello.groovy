88aeb2a5ec94de548824cf5c52ff9ca5b7abb010
hayatamine011 <38259681+hayatamine011@users.noreply.github.com>
2019-03-20 20:49:30 +0000

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,7 +1,7 @@
 // vars/sayHello.groovy
 def call(String a = 'danger') {
 if ( a == "SUCCESS" ) {
-    slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful"
+    slackSend color: "good", message: "*$JOB_NAME*, <$BUILD_URL|Build #$BUILD_NUMBER> Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful"
   }
   else if( a == "FAILURE" ) { 
     slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed"
