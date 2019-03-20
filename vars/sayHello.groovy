74a550103285d8a2a2f61815704511f51abd046c
hayatamine011 <38259681+hayatamine011@users.noreply.github.com>
2019-03-20 18:01:21 +0000

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,8 +1,15 @@
 // vars/sayHello.groovy
-def call(String name = 'human') {
-  if ( name == "SUCCESS" ) {
-// Any valid steps can be called from this code, just like in other // Scripted Pipeline
-echo "Hello, ${name}."
-        slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful"
-
-  }}
+def call(String a = 'human') {
+if ( a == "SUCCESS" ) {
+    slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful"
+  }
+  else if( a == "FAILURE" ) { 
+    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed"
+  }
+  else if( a == "UNSTABLE" ) { 
+    slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable"
+  }
+  else {
+    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its resulat was unclear"	
+  }  
+}
