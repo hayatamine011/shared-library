466b2a1b6584e2618a5d64fd028dc65324148683
hayatamine011 <38259681+hayatamine011@users.noreply.github.com>
2019-03-20 17:57:05 +0000

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,5 +1,6 @@
 // vars/sayHello.groovy
 def call(String name = 'human') {
+  if ( name == "SUCCESS" ) {
 // Any valid steps can be called from this code, just like in other // Scripted Pipeline
 echo "Hello, ${name}."
-}
+  }}
