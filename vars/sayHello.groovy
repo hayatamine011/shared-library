8fc789221b8df36eaaeac89c7700400b345f609e
hayatamine011 <38259681+hayatamine011@users.noreply.github.com>
2019-03-20 17:59:39 +0000

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -3,4 +3,6 @@ def call(String name = 'human') {
   if ( name == "SUCCESS" ) {
 // Any valid steps can be called from this code, just like in other // Scripted Pipeline
 echo "Hello, ${name}."
+        slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful"
+
   }}
